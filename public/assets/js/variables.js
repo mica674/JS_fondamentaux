5b1eab339182189090a60ea23de8d9795b0a31e3
mica674 <mica.fournier@hotmail.fr>
2022-11-17 14:51:27 +0100

    finish

---
+++ public/assets/js/variables.js
@@ -0,0 +1,26 @@
+// Exercice 1
+let message = 'Message à afficher dans la console JS';
+
+console.log(message);
+
+// Exercice 2
+let firstPart = 'Message à afficher ';
+let lastPart = 'dans la console JS';
+
+console.log(firstPart+lastPart);
+
+// Exercice 3
+let firstname = 'Jean';
+let age = 30;
+let city = 'Paris';
+
+console.log(`Bonjour, je m'appelle ${firstname}, j'ai ${age} ans et j'habite à ${city}.`)
+
+// Exercice 4
+let num = 43;
+let txt = '7';
+let sum;
+
+sum = num+txt;
+
+console.log(sum)
